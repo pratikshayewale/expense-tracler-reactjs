58ed6f02c902a5a489071bf2e7ea11270e3bd54e
pratikshayewale <Pratikshayewale880@gmail.com>
2020-05-12 01:17:19 +0530

    Updates

---
+++ precache-manifest.00bc7d4d84906a78b56a8cf67054b947.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62327cdafbf410448c2272ce6bbc20f1",
+    "url": "/expense-tracker-react/index.html"
+  },
+  {
+    "revision": "9d6d13e22219bb137b19",
+    "url": "/expense-tracker-react/static/css/main.cd978270.chunk.css"
+  },
+  {
+    "revision": "f35695db11317516d110",
+    "url": "/expense-tracker-react/static/js/2.c3734ca7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/expense-tracker-react/static/js/2.c3734ca7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d6d13e22219bb137b19",
+    "url": "/expense-tracker-react/static/js/main.883bf9d3.chunk.js"
+  },
+  {
+    "revision": "38d28e6675620d7dc11e",
+    "url": "/expense-tracker-react/static/js/runtime-main.bfd9c6ea.js"
+  }
+]);
